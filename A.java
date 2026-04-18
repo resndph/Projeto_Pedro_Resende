a2516beb1bf09706cc2c6905e2f31cdf8d392920
Pedro Fonseca <phresende203@gmail.com>
2026-04-18 09:13:50 -0300

    Cria classes A e B

---
+++ A.java
@@ -0,0 +1,30 @@
+public class A{
+    private int A1;
+    private float A2;
+
+    public int getA1(){
+        return A1;
+    }
+
+    public float getA2(){
+        return A2;
+    }
+
+    public void setA1(int value){
+        this.A1 = value;
+    }
+
+    public void setA2(float value){
+        this.A2 = value;
+    }
+
+    public void MA1(){
+        System.out.printIn("MA1")
+    }
+
+    public void MA2(){
+        System.out.printIn("MA2")
+    }
+
+    private:
+}
---
+++ B.java
@@ -0,0 +1,30 @@
+public class B{
+    private int B1;
+    private float B2;
+
+    public int getB1(){
+        return B1;
+    }
+
+    public float getB2(){
+        return B2;
+    }
+
+    public void setB1(int value){
+        this.B1 = value;
+    }
+
+    public void setB2(float value){
+        this.B2 = value;
+    }
+
+    public void MB1(){
+        System.out.printIn("MB1")
+    }
+
+    public void MB2(){
+        System.out.printIn("MB2")
+    }
+
+    private:
+}
