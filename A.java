e089d871a05580cff458a454284671fd3deb0743
Pedro Resende <phresende203@gmail.com>
2026-04-19 18:57:43 -0300

    Adiciona getSoma na classe A

--- A.java
+++ A.java
@@ -28,7 +28,10 @@ public class A{
 
     public void MA3() {
     System.out.println("Alteração a classe A partir do clone");
-}
+    }
 
+    public int getSoma(int a, int b) {
+        return a + b;
+    }
     private:
 }
